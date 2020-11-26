dd50a7c58bfe1eccf720055922b26da268bd0b3a
dwkns <dwkns@me.com>
2020-11-26 11:12:06 +0000

    Added notes to config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,3 +10,17 @@ module.exports = {
   variants: {},
   plugins: [require("tailwindcss-debug-screens")]
 }
+
+
+
+// // tailwind.config.js -> To set your own breakpoints
+// module.exports = {
+//   theme: {
+//     screens: {
+//       sm: { min: "640px", max: "767px" },
+//       md: { min: "768px", max: "1023px" },
+//       lg: { min: "1024px", max: "1279px" },
+//       xl: { min: "1280px" },
+//     },
+//   },
+// };
